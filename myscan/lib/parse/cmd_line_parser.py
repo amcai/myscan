e0e270508e9314d443a9c0cd8cd95aa18c9dbbe5
amcai <ai1102a428+mygithubproject@gmail.com>
2020-11-04 11:33:10 +0800

    edit --disable --enable

--- myscan/lib/parse/cmd_line_parser.py
+++ myscan/lib/parse/cmd_line_parser.py
@@ -40,13 +40,14 @@ def cmd_line_parser(argv=None):
         hostscan = parser.add_argument_group('hostscan', "Config hostscan")
         hostscan.add_argument("--input-nmapxml", dest="input_nmapxml", type=str, default=None, help="从nmap -oX 报告输入")
         hostscan.add_argument("--input-nmaptext", dest="input_nmaptext", type=str, default=None, help="从nmap -oN 报告输入")
-        hostscan.add_argument("--input-jsonfile", dest="input_jsonfile", type=str, default=None, help="从指定文件格式输入,格式参考 'docs/Class3-hostscan开发指南.md'")
+        hostscan.add_argument("--input-jsonfile", dest="input_jsonfile", type=str, default=None,
+                              help="从指定文件格式输入,格式参考 'docs/Class3-hostscan开发指南.md'")
         pocs = parser.add_argument_group('pocs', "Config pocs args and pocs to targets")
         pocs.add_argument("--disable", dest="disable", nargs='+', default=[],
-                          help="默认全部开启POC，使用此参数代表禁止de POCS，可根据POC文件名来填写，e.g. --disable xss sqli un_auth，可使用--disable all 代表关闭全部，此时常配合--plugins 使用")
+                          help="默认全部开启POC，使用此参数代表禁止的POCS，根据POC文件名包含字符串判断，e.g. --disable xss sqli un_auth，可使用--disable all 代表关闭全部，此时常配合--plugins 使用")
         pocs.add_argument("--enable", dest="enable", nargs='+', default=[],
-                          help="默认全部开启POC，使用此参数代表仅开启的POCS，可根据POC文件名来填写，e.g. --enable xss sqli un_auth"
-                               "use --enable --disable together,will --enable will not take effect")
+                          help="默认全部开启POC，使用此参数代表仅开启的POCS，根据POC文件名包含字符串判断，e.g. --enable xss sqli un_auth"
+                               "则POC名字包含xss,sqli,un_auth则都会开启,如果同时使用 --enable --disable ， --enable将不会起作用")
         pocs.add_argument("--dishost", dest="dishost", nargs='+',
                           default=["baidu.com", "google.com", "firefox.com", "mozilla.org", "bdstatic.com",
                                    "mozilla.com"],
@@ -61,7 +62,8 @@ def cmd_line_parser(argv=None):
 
         request = parser.add_argument_group('Request', "配置请求参数")
         request.add_argument("--retry", dest="retry", type=int, default=0, help="定义全局request出错后重新尝试请求次数，默认0")
-        request.add_argument("--ipv6", dest="ipv6", action="store_true", help="需网络支持ipv6，使用此参数优先ipv6地址,ipv6无记录再使用ipv4地址")
+        request.add_argument("--ipv6", dest="ipv6", action="store_true",
+                             help="需网络支持ipv6，使用此参数优先ipv6地址,ipv6无记录再使用ipv4地址")
 
         # request.add_argument("--cookie", dest="cookie", default=None, help="测试越权使用cookie，一般为低权限cookie")
         # request.add_argument("--timeout", dest="timeout", type=int, default=None,

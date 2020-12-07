5e1d4cf26e812f4e74d3ea557795f28a34f9e36e
amcai <ai1102a428+mygithubproject@gmail.com>
2020-12-07 16:07:47 +0800

    fix referer

--- myscan/lib/core/options.py
+++ myscan/lib/core/options.py
@@ -234,6 +234,7 @@ def gethtmlheader():
 <head>
     <title>Myscan Report</title>
     <meta charset="utf-8">
+    <meta name="referrer" content="never">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <style>
         body {
---
+++ myscan/pocs/perfolder/shiro/poc_shiro_rce_2019.py
@@ -0,0 +1,4 @@
+# !/usr/bin/env python3
+# @Time    : 2020/12/7
+# @Author  : caicai
+# @File    : poc_shiro_rce_2019.py
